35e95a170def2e50b87aea7a77fc1fcd9100ae8f
Aws ben gandouz <94719195+Awsgandouz@users.noreply.github.com>
2022-03-26 21:17:40 +0100

    Create Wheel factorization.cpp
    This is an optimization of the trial division which is an algorithm to find a prime factorization.  O(n^(1/2))

---
+++ MATH/Wheel factorization.cpp
@@ -0,0 +1,35 @@
+vector<long long> fact(long long n) {
+    vector<long long> factorization;
+    while (n % 2 == 0) {
+        factorization.push_back(2);
+        n /= 2;
+    }
+    for (long long d = 3; d * d <= n; d += 2) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+    }
+    if (n > 1)
+        factorization.push_back(n);
+    return factorization;
+}
+
+// for counting each prime in the decomposition we can use a map
+
+map<int ,int> fact(long long n) {
+    map<int ,int> factorization;
+    while (n % 2 == 0) {
+        factorization[2]++;
+        n /= 2;
+    }
+    for (long long d = 3; d * d <= n; d += 2) {
+        while (n % d == 0) {
+            factorization[d]++;
+            n /= d;
+        }
+    }
+    if (n > 1)
+        factorization[n]++;
+    return factorization;
+}
