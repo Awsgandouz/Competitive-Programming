4ee800286c4bc4292e7a2a43fc0b1a16eac1fbb4
Aws ben gandouz <94719195+Awsgandouz@users.noreply.github.com>
2022-03-26 22:04:53 +0100

    Create BFS.cpp
    Breadth first search is one of the basic and essential searching algorithms on graphs.
    As a result of how the algorithm works, the path found by breadth first search to any node is the shortest path to that node, i.e the path that contains the smallest number of edges in unweighted graphs. 
    The algorithm works in O(N+M) time, where N is number of vertices and M is the number of edges.

---
+++ GRAPHS/Graph traversal/BFS.cpp
@@ -0,0 +1,38 @@
+vector<int> adj[200000];  // adjacency list representation
+int n; // number of nodes
+int s; // source vertex
+
+queue<int> q;
+vector<bool> used(n);
+vector<int> d(n), p(n);
+
+q.push(s);
+used[s] = true;
+p[s] = -1;
+while (!q.empty()) {
+    int v = q.front();
+  // work here !! 
+    q.pop();
+    for (int u : adj[v]) {
+        if (!used[u]) {
+            used[u] = true;
+            q.push(u);
+            d[u] = d[v] + 1;
+            p[u] = v;
+        }
+    }
+}
+
+//If we have to restore and display the shortest path from the source to some vertex u
+// we can add this part 
+if (!used[u]) {
+    cout << "No path!";
+} else {
+    vector<int> path;
+    for (int v = u; v != -1; v = p[v])
+        path.push_back(v);
+    reverse(path.begin(), path.end());
+    cout << "Path: ";
+    for (int v : path)
+        cout << v << " ";
+}
